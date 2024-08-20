0b9b7ae474bbad02cb9c994bbeb9da80da9e75c7
jdheredian <166655174+jdheredian@users.noreply.github.com>
2024-08-20 08:39:45 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,24 +1,52 @@
-document.getElementById('notion-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    // Obtener los valores del formulario
-    const title = document.getElementById('notion-title').value;
-    const url = document.getElementById('notion-url').value;
-  
-    // Crear un nuevo elemento de lista
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.textContent = title;
-    a.href = url;
-    a.target = "_blank";
-  
-    // Agregar el enlace al elemento de lista
-    li.appendChild(a);
-  
-    // Agregar el elemento de lista a la lista de enlaces de Notion
-    document.getElementById('notion-links').appendChild(li);
-  
-    // Limpiar el formulario
-    document.getElementById('notion-title').value = '';
-    document.getElementById('notion-url').value = '';
+// Mostrar/ocultar secciones específicas basadas en los clics en la barra lateral
+function showSection(sectionId) {
+  // Ocultar todas las secciones
+  var sections = document.querySelectorAll('.section-content');
+  sections.forEach(function(section) {
+      section.style.display = 'none';
   });
+
+  // Mostrar la sección seleccionada
+  var sectionToShow = document.getElementById(sectionId);
+  if (sectionToShow) {
+      sectionToShow.style.display = 'block';
+  }
+}
+
+// Mostrar la sección de bienvenida por defecto cuando se carga la página
+document.addEventListener('DOMContentLoaded', function() {
+  var welcomeSection = document.getElementById('welcome');
+  if (welcomeSection) {
+      welcomeSection.style.display = 'block';
+  }
+
+  // Seleccionar el botón de Dark Mode
+  const toggleButton = document.getElementById('toggle-dark-mode');
+  const modeIcon = document.getElementById('mode-icon');
+
+  // Verificar si hay un tema guardado en localStorage
+  if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark-mode');
+      modeIcon.classList.remove('fa-moon');
+      modeIcon.classList.add('fa-sun'); // Cambiar icono a sol
+  }
+
+  // Manejar el evento de clic en el botón
+  if (toggleButton && modeIcon) { // Verifica que los elementos existan
+      toggleButton.addEventListener('click', function() {
+          document.body.classList.toggle('dark-mode');
+
+          // Cambiar el icono y guardar la preferencia en localStorage
+          if (document.body.classList.contains('dark-mode')) {
+              modeIcon.classList.remove('fa-moon');
+              modeIcon.classList.add('fa-sun'); // Cambiar a icono de sol
+              localStorage.setItem('theme', 'dark');
+          } else {
+              modeIcon.classList.remove('fa-sun');
+              modeIcon.classList.add('fa-moon'); // Cambiar a icono de luna
+              localStorage.setItem('theme', 'light');
+          }
+      });
+  }
+});
+
